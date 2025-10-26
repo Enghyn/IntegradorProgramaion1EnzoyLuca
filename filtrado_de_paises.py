9339d1e1eef260234d8d1ec33679dc1eea8f08be
Ruka77 <luca.argumedo@gmail.com>
2025-10-26 17:05:26 -0300

    funciones terminadas

--- filtrado_de_paises.py
+++ filtrado_de_paises.py
@@ -1,11 +1,43 @@
-def filtrar_por_continente():
-    pass
+import csv
+ruta = "archivo_paices.csv"
 
-def filtrar_por_rango_de_población():
-    pass
+#se le pasa por parametro el continente que se desea buscar y la función filtra entre toda la lista del csv para obtener los paices cuyos continentes son iguales al buscado
+def filtrar_por_continente(continente):
+    paices = []
 
-def filtrar_por_rango_de_superficie():
-    pass
+    with open(ruta, newline="", encoding="utf-8") as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            if fila["continente"].strip().lower() == continente:
+                paices.append(fila["nombre"], fila["poblacion"], fila["superficie"], fila["continente"])
+
+    return paices
+
+#se pasa por parametro la población minima y maxima de lo que se deséa filtrar, luego la función busca todos los paices que se encuentren entre esos dos valores y devuelve una tupla de estos
+def filtrar_por_rango_de_población(poblacion_min, poblacion_max):
+    paises = []
+
+    with open(ruta, newline="", encoding="utf-8") as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+                poblacion = float(fila["poblacion"])
+                if poblacion_min <= poblacion <= poblacion_max:
+                    paises.append((fila["nombre"], poblacion, fila["superficie"], fila["continente"]))
+
+    return paises
+
+#se pasa por parametro la superficie minima y maxima de lo que se deséa filtrar, luego la función busca todos los paices que se encuentren entre esos dos valores y devuelve una tupla de estos
+def filtrar_por_rango_de_superficie(superficie_min, superficie_max):
+    paises = []
+
+    with open(ruta, newline="", encoding="utf-8") as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+                superficie = float(fila["superficie"])
+                if superficie_min <= superficie <= superficie_max:
+                    paises.append((fila["nombre"], fila["poblacion"], superficie, fila["continente"]))
+
+    return paises
 
 def main():
     filtrar_por_continente()
