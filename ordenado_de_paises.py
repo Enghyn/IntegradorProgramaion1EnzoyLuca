5f061462eff8e41414419773e88f44ed98da1a61
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-10-26 17:25:10 -0300

    funcionalidad metodos ordenamiento completados

--- ordenado_de_paises.py
+++ ordenado_de_paises.py
@@ -1,8 +1,78 @@
-def ordenar_por_nombre(lista_nombres:list):
-    pass
+import csv
+
+RUTA = "archivo_paices.csv"
+
+def obtener_paises():
+    """
+    Función general que lee el archivo csv y devuelve una lista con todos los elementos del csv
+    """
+    try:
+        lista_paises = []
+
+        with open(RUTA, "r", newline="", encoding="UTF-8") as archivo:
+            lector = csv.reader(archivo)
+            next(lector)
+            for linea in lector:
+                lista_paises.append(linea)
+        
+        return lista_paises
+    
+    except:
+        print("Error")
+
+def ordenar_por_nombre():
+    """
+    Función que sobreescribe el csv por nombre, de manera ascendente (A, B, C, D, etc.)
+    """
+    try:
+        lista_paises = obtener_paises()
+        
+        lista_paises.sort(key=lambda pais: pais[0])
+        #El método sort ordena la lista en base al primer elemento de cada sub-lista
+        
+        with open(RUTA, "w", newline="", encoding="UTF-8") as archivo:
+            escritor = csv.writer(archivo)
+            escritor.writerow(["nombre","poblacion","superficie","continente"])
+            escritor.writerows(lista_paises)
+    except:
+        print("Error")
 
 def ordenar_por_poblacion():
+    """
+    Función que sobreescribe el csv ordenando los países por población de manera ascendente
+    """
+    try:
+        lista_paises = obtener_paises()
+
+        lista_paises.sort(key=lambda pais: int(pais[1]))
+        #El método sort ordena la lista en base al segundo elemento de cada sub-lista
+
+        with open(RUTA, "w", newline="", encoding="UTF-8") as archivo:
+            escritor = csv.writer(archivo)
+            escritor.writerow(["nombre","poblacion","superficie","continente"])
+            escritor.writerows(lista_paises)
+    except:
+        print("Error")
+
+def ordenar_por_superficie(descendente:bool):
+    """
+    Función que sobreescribe el csv ordenando los países por superficie de manera ascendente
+    """
+    try:
+        lista_paises = obtener_paises()
+
+        lista_paises.sort(reverse=descendente, key=lambda pais: float(pais[2]))
+        #El método sort ordena la lista en base al tercer elemento de cada sub-lista
+
+        with open(RUTA, "w", newline="", encoding="UTF-8") as archivo:
+            escritor = csv.writer(archivo)
+            escritor.writerow(["nombre","poblacion","superficie","continente"])
+            escritor.writerows(lista_paises)
+    except:
+        print("Error")
+
+def main():
     pass
 
-def ordenar_por_superficie():
-    pass
+if __name__ == "__main__":
+    main()
