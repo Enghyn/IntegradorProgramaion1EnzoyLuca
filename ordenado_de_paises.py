4175e3a6f423713896cd82d8fc967208e408457a
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-10-27 08:05:26 -0300

    agregado comentarios ordenado_de_paises.py

--- ordenado_de_paises.py
+++ ordenado_de_paises.py
@@ -2,10 +2,8 @@ import csv
 
 RUTA = "archivo_paices.csv"
 
+#Función general que lee el archivo csv y devuelve una lista con todos los elementos del csv
 def obtener_paises():
-    """
-    Función general que lee el archivo csv y devuelve una lista con todos los elementos del csv
-    """
     try:
         lista_paises = []
 
@@ -20,19 +18,15 @@ def obtener_paises():
     except:
         print("Error")
 
+#Función general que carga una lista de paises en el csv
 def cargar_paises(lista_paises:list):
-    """
-    Función general que carga una lista de paises en el csv
-    """
     with open(RUTA, "w", newline="", encoding="UTF-8") as archivo:
             escritor = csv.writer(archivo)
             escritor.writerow(["nombre","poblacion","superficie","continente"])
             escritor.writerows(lista_paises)
 
+#Función que sobreescribe el csv por nombre, de manera ascendente (A, B, C, D, etc.)
 def ordenar_por_nombre():
-    """
-    Función que sobreescribe el csv por nombre, de manera ascendente (A, B, C, D, etc.)
-    """
     try:
         lista_paises = obtener_paises()
         
@@ -43,10 +37,8 @@ def ordenar_por_nombre():
     except:
         print("Error")
 
+#Función que sobreescribe el csv ordenando los países por población de manera ascendente
 def ordenar_por_poblacion():
-    """
-    Función que sobreescribe el csv ordenando los países por población de manera ascendente
-    """
     try:
         lista_paises = obtener_paises()
 
@@ -57,10 +49,8 @@ def ordenar_por_poblacion():
     except:
         print("Error")
 
+#Función que sobreescribe el csv ordenando los países por superficie de manera ascendente
 def ordenar_por_superficie(descendente:bool):
-    """
-    Función que sobreescribe el csv ordenando los países por superficie de manera ascendente
-    """
     try:
         lista_paises = obtener_paises()
 
